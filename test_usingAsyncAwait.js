c173350d38c9504f30219048346b1128ca1e655b
ssarun9871 <ssarun9871@gmail.com>
2022-12-15 17:31:16 +0530

    adding try and catch

--- test_usingAsyncAwait.js
+++ test_usingAsyncAwait.js
@@ -6,55 +6,82 @@ let btn = document.getElementById('submit');
 let display = document.getElementById('display');
 btn.addEventListener('click', addExpense);
 
+
 async function onStart() {
-    let res  = await axios.get('https://crudcrud.com/api/9b833b9c18614580ade7a4aa3a4a6069/data')
+    try{
+        let res  = await axios.get('https://crudcrud.com/api/659b68585873408da7365aeb563c75f6/data')
        
-    for (let i = 0; i < res.data.length; i++) {
-        let id = res.data[i]._id;
-        let exp = `${res.data[i].amount}-${res.data[i].description}-${res.data[i].category}`;
-        displayOnScreen(id, exp)
+        for (let i = 0; i < res.data.length; i++) {
+            let id = res.data[i]._id;
+            let exp = `${res.data[i].amount}-${res.data[i].description}-${res.data[i].category}`;
+            displayOnScreen(id, exp)
+        }
     }
+    catch(err){
+        console.log(err)
+    }
+
 }
 
 
 async function addExpense(e) {
     e.preventDefault();
+    try{
+        if(amount.value==''||description.value=='') return false;
 
-    let obj = {
-        amount: amount.value,
-        description: description.value,
-        category: category.value
-    };
-    let exp = `${amount.value}-${description.value}-${category.value}`;
+        let obj = {
+            amount: amount.value,
+            description: description.value,
+            category: category.value
+        };
+        let exp = `${amount.value}  -  ${description.value}  -  ${category.value}`;
+    
+        const res= await axios.post('https://crudcrud.com/api/659b68585873408da7365aeb563c75f6/data', obj)
+        displayOnScreen(res.data._id, exp);
+    }
 
-    const res= await axios.post('https://crudcrud.com/api/9b833b9c18614580ade7a4aa3a4a6069/data', obj)
-    displayOnScreen(res.data._id, exp);
+    catch(err){
+        console.log(err);
+    }
+
 }
 
 
 function displayOnScreen(id, expense) {
-    let p = `<li id="${id}">${expense}  <button onClick="deleteExpense('${id}')">Delete Expense</button>  <button onClick = "editExpense('${id}')">Edit Expense</button></li>`;
+    let p = `<li id="${id}" style="list-style:none;display:block;height:40px">${expense} <div style="float:right"> <button class="btn btn-secondary btn-sm" style="border-radius:20px; margin-right:10px" onClick="deleteExpense('${id}')">Delete Expense</button>  <button class="btn btn-secondary btn-sm" style="border-radius:20px" onClick = "editExpense('${id}')">Edit Expense</button></div></li>`;
     display.innerHTML = display.innerHTML + p;
 }
 
 
 function deleteExpense(id) {
-    let elementToRemove = document.getElementById(id);
-    elementToRemove.remove();
-    return axios.delete(`https://crudcrud.com/api/9b833b9c18614580ade7a4aa3a4a6069/data/${id}`)
+    try{
+        let elementToRemove = document.getElementById(id);
+        elementToRemove.remove();
+        return axios.delete(`https://crudcrud.com/api/659b68585873408da7365aeb563c75f6/data/${id}`)
+    }
+
+    catch(err){
+     console.log(err)
+    }
+
 }
 
 
 async function editExpense(id) {
-    let res = await axios.get(`https://crudcrud.com/api/9b833b9c18614580ade7a4aa3a4a6069/data/${id}`)
+    try{   
+        let res = await axios.get(`https://crudcrud.com/api/659b68585873408da7365aeb563c75f6/data/${id}`)
+        let amt = res.data.amount;
+        let des = res.data.description;
+        let cat = res.data.category;
 
-    let amt = res.data.amount;
-    let des = res.data.description;
-    let cat = res.data.category;
+        amount.value = amt,
+        description.value = des,
+        category.value = cat;
 
-    amount.value = amt,
-    description.value = des,
-    category.value = cat;
+        deleteExpense(id)
+    }
 
-    deleteExpense(id)
+    catch(err){
+        console.log(err);
+    }
 }
