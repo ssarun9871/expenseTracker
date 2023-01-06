43674bed3f120844bcc84dc0c52909140c3c0547
ssarun9871 <ssarun9871@gmail.com>
2023-01-06 15:04:15 +0530

    adding password encryption

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,50 @@
 const Users = require('../models/users');
+const bcrypt = require('bcrypt');
 
-exports.postAddUsers=async (req,res,next)=>{
-    await Users.create({
-       name:req.body.name,
-       email:req.body.email,
-       password: req.body.password
-    })
-    .then(result=> res.json("User added successfully"))
-    .catch(err => {
-        if(err.errors[0].validatorKey=="not_unique"){
-            res.status(400).send("already_exist");
-        }
-        else {
-            res.status(400).send(err);
-        }
+exports.postAddUsers= (req,res,next)=>{
+    let password = req.body.password;
+    bcrypt.hash(password, 10).then( async (hash)=> {
+        await Users.create({
+            name:req.body.name,
+            email:req.body.email,
+            password: hash
+         })
+         .then(result=> res.json("User added successfully"))
+         .catch(err => {
+             if(err.errors[0].validatorKey=="not_unique"){
+                 res.status(400).send("already_exist");
+             }
+             else {
+                 res.status(400).send(err);
+             }
+         });
     });
+
 }
+
 
 exports.postLoginUser = async (req,res,next)=>{
     let email = req.body.email;
-    let password = req.body.password;
+    let passwordFromUser = req.body.password;
     let user = await Users.findByPk(email);
+
+    //if user exists in the database
     if(user){
-        if(user.password==password){
+        let passwordFromDB = user.password
+        const match = await bcrypt.compare(passwordFromUser, passwordFromDB);
+        
+        //if both the passwords matches
+        if(match){
             res.json({status:"successful", message:"User login successful"});
         }
+        //if passwords doesn't matches
         else{
-            res.status(401).json({status:"failed", message:"User not authorized"});
-           }
+            res.status(400).json({status:"failed", message:"User not authorized"});
         }
+        }
+    //if user doesn't exists in the database    
     else{
-        res.status(401).json({status:"failed" , message:"User not found"});
+        res.status(404).json({status:"failed" , message:"User not found"});
         }
 
 }
